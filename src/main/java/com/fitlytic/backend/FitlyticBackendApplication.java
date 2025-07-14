4318021a390479e000549d55ada8dcfec3ee67a2
Harry Sturgeon <harrysturgeon97@gmail.com>
2025-07-14 14:07:48 +0100

    Initial Commit

---
+++ src/main/java/com/fitlytic/backend/FitlyticBackendApplication.java
@@ -0,0 +1,13 @@
+package com.fitlytic.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitlyticBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FitlyticBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fitlytic/backend/FitlyticBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fitlytic.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitlyticBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
